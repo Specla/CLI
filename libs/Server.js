c24ed5d1b5076572ad810608eb785f8bff3efccb
Frederik Kvartborg <fka@skemax.dk>
2016-09-18 12:26:21 +0200

    Split the server and specla setup into seperate files

--- libs/Server.js
+++ libs/Server.js
@@ -2,8 +2,6 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const Database = require('specla-database');
-const Autoloader = require('specla-autoloader');
 
 class Server {
 
@@ -11,58 +9,41 @@ class Server {
     this.app = null;
     this.express = express;
     this.config = config;
-    this.events = this.defaultEvents();
 
     this.setupExpress();
-    this.setupSpecla();
     this.setupRoutes();
   }
 
-  defaultEvents(){
-    return {
-      on: {
-        start: () => {}
-      }
-    }
-  }
-
+  /**
+   * Setup express
+   * @private
+   */
   setupExpress(){
     this.app = express();
+    Specla.express = this.app;
 
     this.app.use(express.static('public'));
     this.app.use(bodyParser.urlencoded({ extended: true }));
     this.app.set('view engine', this.config.view.engine);
     this.app.set('views', this.config.view.path);
-
-    //require('./ErrorHandler');
   }
 
-  setupSpecla(){
-    global.DB = new Database({
-      host: this.config.database.host,
-      port: this.config.database.port,
-      database: this.config.database.database
-    });
-
-    new Autoloader(this.config.autoloader.global).global();
-  }
-
+  /**
+   * Setup the router (default express router)
+   * @private
+   */
   setupRoutes(){
     global.Route = this.app;
     require(process.cwd()+'/app/routes');
     global.Route = undefined;
   }
 
-  on(event, callback){
-    this.events.on[event] = callback;
-  }
-
-  trigger(event, data){
-    this.events.on[event](data);
-  }
-
+  /**
+   * Start the HTTP server
+   * @param {Function} callback
+   * @public
+   */
   listen(callback){
-    this.trigger('start', this.app);
     this.app.listen(this.config.server.port);
 
     if(typeof callback === 'function'){
---
+++ libs/Specla.js
@@ -0,0 +1,111 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const Database = require('specla-database');
+const Autoloader = require('specla-autoloader');
+const Server = require('./Server');
+
+class Specla {
+
+  constructor(config){
+    this.config = config;
+    this.express = null;
+    this.modules = {};
+    this.models = {};
+    this.events = {};
+
+    global.Specla = this;
+
+    this.setupDefaultEvents();
+    this.setupSpecla();
+  }
+
+  /**
+   * Setup default events
+   * @private
+   */
+  setupDefaultEvents(){
+    process.on('exit', this.trigger.bind(this, 'exit'));
+    process.on('SIGINT', () => {
+      this.trigger.bind(this, 'exit');
+      process.exit();
+    });
+  }
+
+  /**
+   * Setup Specla and its modules
+   * @private
+   */
+  setupSpecla(){
+    this.modules.DB = new Database({
+      host: this.config.database.host,
+      port: this.config.database.port,
+      database: this.config.database.database
+    });
+
+    new Autoloader(this.config.autoloader.global).global();
+  }
+
+  /**
+   * Register event listener
+   * @param {String}    event
+   * @param {Function}  callback
+   * @return {Object}   this
+   * @public
+   */
+  on(event, callback){
+    if(this.events[event] === undefined){
+      this.events[event] = [];
+    }
+
+    this.events[event].push(callback);
+
+    return this;
+  }
+
+  /**
+   * Trigger event listener
+   * @param {String}  event
+   * @param {Mixed}   data
+   * @return {Object} this
+   * @public
+   */
+  trigger(event, ...data){
+    if(data.length === 1){
+      data = data[0];
+    }
+
+    if(data.length === 0){
+      data = undefined;
+    }
+
+    if(this.events[event] === undefined){
+      return this;
+    }
+
+    for(let _event of this.events[event]){
+      _event.apply(data);
+    }
+
+    return this;
+  }
+
+  /**
+   * Start the HTTP server from the server module
+   * @param {Function} callback
+   * @public
+   */
+  listen(callback){
+    const server = new Server(this.config);
+    server.listen(() => {
+      this.trigger('ready');
+
+      if(callback !== undefined){
+        callback();
+      }
+    });
+  }
+}
+
+module.exports = Specla;
