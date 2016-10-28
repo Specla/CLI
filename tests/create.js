6593b8e98bf9015268693e91348dd8bace23c8d1
Frederik Kvartborg <fka@skemax.dk>
2016-10-28 22:36:06 +0200

    Added tests

---
+++ tests/create.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const { exec } = require('child_process');
+
+describe('# Create new specla project', () => {
+
+  before((done) => {
+    exec('npm link', () => {
+      done();
+    });
+  });
+
+  it('should download the framework from github and install npm dependencies', function(done) {
+    this.timeout(10000*6);
+
+    exec('specla create tests/project', (err) => {
+      if(err !== null){
+        return console.log(err);
+      }
+      done();
+    });
+  });
+
+  after((done) => {
+    let currentDir = __dirname;
+    exec('cd tests/project; npm link specla', () => {
+      done();
+    });
+  });
+});
---
+++ tests/index.js
@@ -0,0 +1,5 @@
+const { exec } = require('child_process');
+
+exec('npm link');
+
+require('./create');
