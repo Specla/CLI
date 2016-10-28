adb094fef58e6f438fab234827b40bb4d01c2a06
Frederik Kvartborg <fka@skemax.dk>
2016-10-28 21:49:11 +0200

    Removed package info from global scope

--- bin/specla.js
+++ bin/specla.js
@@ -6,14 +6,11 @@ const Specla = require('specla');
 const SuperCLI = require('super-cli');
 const CommandLoader = require('./libs/CommandLoader');
 
-global.packageInfo = require('../package.json');
-
 const cli = new SuperCLI({
   name: 'Specla'
 });
 
 new CommandLoader(cli);
-
 
 cli.on('missing', () => {
   if(cli.has(['-v', '--version'])){
