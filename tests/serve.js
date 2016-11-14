c0807b733b93b6f59cab27be42dcad989fd1ca2c
Frederik Kvartborg <fka@skemax.dk>
2016-11-14 21:54:52 +0100

    Fixed the server issue related to the serve test

--- tests/serve.js
+++ tests/serve.js
@@ -2,20 +2,24 @@ const assert = require('assert');
 const request = require('request');
 const { spawn } = require('child_process');
 
+let server;
+
 describe('# Command: serve', () => {
 
-  it('Should start the web server', function(done) {
-    this.timeout(10000);
-
-    let server = spawn('specla', ['serve']);
-
-    setTimeout(() => {
-      request('http://localhost:3000', (err, res, body) => {
-        assert.equal(200, res.statusCode);
-        server.kill();
-        done();
-      });
-    }, 3000);
+  before(done => {
+    server = spawn('node', ['server.js']);
+    setTimeout(done, 1000);
   });
 
+  it('Should start the web server', function(done) {
+    request('http://localhost:3000', (err, res, body) => {
+      if(err) throw err;
+      assert.equal(200, res.statusCode);
+      done();
+    });
+  });
+
+  after(() => {
+    server.kill();
+  })
 });
