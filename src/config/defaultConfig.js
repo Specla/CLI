6343118cdb10348e19cf24e65190f00bfbc22f70
kvartborg <hello@kvartborg.net>
2017-11-09 13:34:59 +0100

    Update default config

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,10 +1,17 @@
 export default {
   'app.name': 'specla',
+  'app.port': 3000,
   specla: {
     'config.path': './config',
     'controller.path': './api/controllers',
     'command.path': './api/commands',
     'model.path': './api/models',
-    'view.path': './src/views'
+    'view.path': './src/views',
+    'public.path': './public',
+    storage: {
+      path: './storage',
+      app: './storage/app',
+      cache: './storage/cache'
+    }
   }
 }
